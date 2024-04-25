45a1ee1a5c66696345661d9f5bccfb6d6e46fb6d
tuannt20 <tuango.472000@gmail.com>
2024-04-26 00:14:10 +0700

    feat: Update UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
 import React, { useState } from 'react';
-import "./App.css";
+import './App.css';
 import MapComponent from './components/MapComponents';
-
 
 function App() {
   // Define the initial state for the dropdown value
-  const [selectedValue, setSelectedValue] = useState('')
+  const [selectedValue, setSelectedValue] = useState('');
 
   // Handle the dropdown change event
-  const handleDropdownChange = (e) => {
+  const handleDropdownChange = e => {
     setSelectedValue(e.target.value);
-  }
+  };
 
   return (
     <>
       <h1>Demo Map</h1>
 
       {/*Select route component */}
-      <form action="" className='form'>
-      <label htmlFor="chooseRoute">Choose route:</label>
-      <select value={selectedValue} onChange={handleDropdownChange}>
-        <option value="hoianaToDaNang">Hoiana - Da Nang</option>
-        <option value="tamkyToHoiana">Tam Ky - Hoiana</option>
-        <option value="hoianaToHoian">Hoiana - HoiAn</option>
-      </select>
+      <form action='' className='form'>
+        <label htmlFor='chooseRoute'>Choose route:</label>
+        <select value={selectedValue} onChange={handleDropdownChange}>
+          <option value='hoianaToDaNang'>Hoiana - Da Nang</option>
+          <option value='tamkyToHoiana'>Tam Ky - Hoiana</option>
+          <option value='hoianaToHoian'>Hoiana - HoiAn</option>
+        </select>
 
-      <label htmlFor="firstLocation">First location (Realtime location):</label>
-      <input type="text" value={} />
+        <label htmlFor='firstLocation'>
+          First location (Realtime location):
+        </label>
+        <input type='text' />
+        <label htmlFor='secondLocation'>Bus stop location:</label>
+        <input type='text' />
       </form>
       <MapComponent />
     </>
